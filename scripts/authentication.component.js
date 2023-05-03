62787909c0965f51afb16873a55ff1bdd1af9ff3
johnatan quenes dos santos <johnatan.quenes@gmail.com>
2023-05-02 21:24:08 -0300

    modificacao no projeto

---
+++ scripts/authentication.component.js
@@ -0,0 +1,15 @@
+let btnSignin = document.querySelector("#signin");
+let btnSignup = document.querySelector("#signup");
+
+let body = document.querySelector("body");
+
+btnSignin.addEventListener("click", function () {
+  body.className = "sign-in-js";
+});
+
+btnSignup.addEventListener("click", function () {
+  body.className = "sign-up-js";
+});
+
+btnSignin();
+btnSignup();
